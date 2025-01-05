52bab3127e68739cb3161cb7a9f544c6285a3a82
harryphma <phamquanghung2705@gmail.com>
2025-01-04 21:53:18 -0500

    using Input compo

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import Input from "./Input.jsx";
+import {isEmail, isNotEmpty, hasMinLength} from '../util/validation.js';
 export default function Login() {
 
   const [enteredValues, setEnteredValues] = useState({
@@ -12,7 +13,8 @@ export default function Login() {
     password: false
   });
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+  const emailIsInvalid = didEdit.email && !isEmail(enteredValues.email) && !isNotEmpty(enteredValues.email);
+  const passwordIsInvalid = didEdit.password && !hasMinLength(enteredValues.password, 6);
 
   function handleSubmit(event){
     event.preventDefault();
@@ -43,19 +45,10 @@ export default function Login() {
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onBlur={() => handleInputBlur('email')} onChange={(event) => handleInputChange('email', event.target.value)} value={enteredValues.email} />
-        </div>
-
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={(event) => handleInputChange('password', event.target.value)} value={enteredValues.password}/>
-        </div>
+        <Input label="Email" id="email" type="email" name="email" onBlur={() => handleInputBlur('email')} onChange={(event) => handleInputChange('email', event.target.value)} value={enteredValues.email} error={emailIsInvalid && 'Please enter a valid email'}></Input>
+        <Input label="Password" id="password" type="password" name="password" onBlur={() => handleInputBlur('password')} onChange={(event) => handleInputChange('password', event.target.value)} value={enteredValues.password} error={passwordIsInvalid && 'Please enter a valid password'}></Input>
       </div>
       
-      <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
-
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
         <button className="button">Login</button>
