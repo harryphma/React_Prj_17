70c96087d61567e5159d41e28029f3514019045f
harryphma <phamquanghung2705@gmail.com>
2025-01-04 20:36:34 -0500

    valid on keystroke

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -7,6 +7,8 @@ export default function Login() {
     password: ''
   });
 
+  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+
   function handleSubmit(event){
     event.preventDefault();
     console.log(enteredValues);
@@ -34,6 +36,7 @@ export default function Login() {
           <input id="password" type="password" name="password" onChange={(event) => handleInputChange('password', event.target.value)} value={enteredValues.password}/>
         </div>
       </div>
+      <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
